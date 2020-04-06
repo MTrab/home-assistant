988716ea001ec876a24d0f177b0e6bd85c3183f2
Morten Trab <morten@trab.dk>
2020-04-06 11:17:08 +0200

    Trying to get async running

--- homeassistant/components/landroid_cloud/__init__.py
+++ homeassistant/components/landroid_cloud/__init__.py
@@ -1,76 +1,78 @@
 """Support for Worx Landroid Cloud based lawn mowers."""
-import logging
 from datetime import timedelta
+import logging
 
+import async_timeout
 import voluptuous as vol
 
-from homeassistant.const import (
-    CONF_EMAIL, CONF_DEVICE, CONF_PASSWORD)
+from homeassistant.const import CONF_DEVICE, CONF_EMAIL, CONF_PASSWORD
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.discovery import load_platform
 from homeassistant.helpers.dispatcher import dispatcher_send
-from homeassistant.helpers.event import async_track_time_interval 
+from homeassistant.helpers.event import async_track_time_interval
 
 _LOGGER = logging.getLogger(__name__)
 
-DEFAULT_NAME = 'landroid'
-DOMAIN = 'landroid_cloud'
-LANDROID_API = 'landroid_cloud_api'
+DEFAULT_NAME = "landroid"
+DOMAIN = "landroid_cloud"
+LANDROID_API = "landroid_cloud_api"
 SCAN_INTERVAL = timedelta(seconds=10)
-UPDATE_SIGNAL = 'landroid_cloud_update_signal'
+UPDATE_SIGNAL = "landroid_cloud_update_signal"
 
-CONFIG_SCHEMA = vol.Schema({
-    DOMAIN: vol.Schema({
-        vol.Required(CONF_EMAIL): cv.string,
-        vol.Required(CONF_PASSWORD): cv.string,
-        vol.Optional(CONF_DEVICE, default=0): cv.positive_int,
-    })
-}, extra=vol.ALLOW_EXTRA)
+CONFIG_SCHEMA = vol.Schema(
+    {
+        DOMAIN: vol.Schema(
+            {
+                vol.Required(CONF_EMAIL): cv.string,
+                vol.Required(CONF_PASSWORD): cv.string,
+                vol.Optional(CONF_DEVICE, default=0): cv.positive_int,
+            }
+        )
+    },
+    extra=vol.ALLOW_EXTRA,
+)
 
-SERVICE_START = 'start'
-SERVICE_PAUSE = 'pause'
-SERVICE_HOME = 'home'
+SERVICE_START = "start"
+SERVICE_PAUSE = "pause"
+SERVICE_HOME = "home"
 
 
 API_WORX_SENSORS = {
-    'battery': {
-        'state': {
-            'battery_percent': 'state',
-            'battery_voltage': 'battery_voltage',
-            'battery_temperature': 'battery_temperature',
-            'battery_charge_cycles': 'charge_cycles',
-            'battery_charging': 'charging',
+    "battery": {
+        "state": {
+            "battery_percent": "state",
+            "battery_voltage": "battery_voltage",
+            "battery_temperature": "battery_temperature",
+            "battery_charge_cycles": "charge_cycles",
+            "battery_charging": "charging",
         },
-        'icon': 'mdi:battery',
-        'unit': '%',
-        'device_class': None,
+        "icon": "mdi:battery",
+        "unit": "%",
+        "device_class": None,
     },
-    'error': {
-        'state': {
-            'error_description': 'state',
-            'error': 'error_id',
-        },
-        'icon': None,
-        'unit': None,
-        'device_class': None,
+    "error": {
+        "state": {"error_description": "state", "error": "error_id"},
+        "icon": None,
+        "unit": None,
+        "device_class": None,
     },
-    'status': {
-        'state': {
-            'status_description': 'state',
-            'blade_time': 'blade_time',
-            'work_time': 'work_time',
-            'distance': 'distance',
-            'status': 'status_id',
-            'updated': 'last_update',
-            'rssi': 'rssi',
-            'yaw': 'yaw',
-            'roll': 'roll',
-            'pitch': 'pitch',
+    "status": {
+        "state": {
+            "status_description": "state",
+            "blade_time": "blade_time",
+            "work_time": "work_time",
+            "distance": "distance",
+            "status": "status_id",
+            "updated": "last_update",
+            "rssi": "rssi",
+            "yaw": "yaw",
+            "roll": "roll",
+            "pitch": "pitch",
         },
-        'icon': None,
-        'unit': None,
-        'device_class': None,
-    }
+        "icon": None,
+        "unit": None,
+        "device_class": None,
+    },
 }
 
 
@@ -83,9 +85,8 @@ async def async_setup(hass, config):
     cloud_device_id = config[DOMAIN][CONF_DEVICE]
 
     client = pyworxcloud.WorxCloud()
-    auth = await client.initialize(cloud_email,
-                                   cloud_password,
-                                   cloud_device_id)
+    async with async_timeout.timeout(10):
+        auth = await client.initialize(cloud_email, cloud_password, cloud_device_id)
 
     if not auth:
         return False
@@ -98,31 +99,19 @@ async def async_setup(hass, config):
         """Handle start service call."""
         client.start()
 
-    hass.services.async_register(
-        DOMAIN,
-        SERVICE_START,
-        handle_start
-    )
+    hass.services.async_register(DOMAIN, SERVICE_START, handle_start)
 
     async def handle_pause(call):
         """Handle pause service call."""
         client.pause()
 
-    hass.services.async_register(
-        DOMAIN,
-        SERVICE_PAUSE,
-        handle_pause
-    )
+    hass.services.async_register(DOMAIN, SERVICE_PAUSE, handle_pause)
 
     async def handle_home(call):
         """Handle pause service call."""
         client.stop()
 
-    hass.services.async_register(
-        DOMAIN,
-        SERVICE_HOME,
-        handle_home
-    )
+    hass.services.async_register(DOMAIN, SERVICE_HOME, handle_home)
 
     return True
 
@@ -138,27 +127,22 @@ class WorxLandroidAPI:
 
         sensor_info = []
         info = {}
-        info['name'] = '{}_{}'.format(DEFAULT_NAME,
-                                      self._client.name)
-        info['friendly'] = self._client.name
+        info["name"] = "{}_{}".format(DEFAULT_NAME, self._client.name)
+        info["friendly"] = self._client.name
         sensor_info.append(info)
 
-        load_platform(self._hass,
-                            'sensor',
-                            DOMAIN,
-                            sensor_info,
-                            self.config)
+        load_platform(self._hass, "sensor", DOMAIN, sensor_info, self.config)
 
     def get_data(self, sensor_type):
         """Get data from state cache."""
         methods = API_WORX_SENSORS[sensor_type]
         data = {}
-        for prop, attr in methods['state'].items():
+        for prop, attr in methods["state"].items():
             prop_data = getattr(self._client, prop)
             data[attr] = prop_data
         return data
 
     async def async_update(self, now=None):
         """Update the state cache from Landroid API."""
-        #self._client.update()
+        # self._client.update()
         dispatcher_send(self._hass, UPDATE_SIGNAL)
